eca6bdb6a1ca9dff12f84e784981cc228eb18a6f
Donovan Vernon <d.vernon5@gmail.com>
2025-08-04 16:58:07 -0700

    Added a method to append string to a text

--- main.cpp
+++ main.cpp
@@ -5,7 +5,12 @@
 class TextEditor {
     public:
         TextEditor(): text("") {};
-        
+        std::string append(const std::string& str) {
+            undoStack.push(text);
+            text += str;
+            return text;
+        }
+
     private:
         std::string text;
         std::stack<std::string> undoStack;
