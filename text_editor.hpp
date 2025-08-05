c69b8341776bde61c08414e26535d918223f5c8a
Donovan Vernon <d.vernon5@gmail.com>
2025-08-04 17:16:06 -0700

    Separated the object into its own file

---
+++ text_editor.hpp
@@ -0,0 +1,20 @@
+#ifndef TEXT_EDITOR_HPP
+#define TEXT_EDITOR_HPP
+
+#include <string>
+#include <stack>
+
+class TextEditor {
+    public:
+        TextEditor();
+        std::string append(const std::string& str);
+        std::string deleteChar(size_t index);
+        void printChars(size_t count);
+        void undo();
+
+    private:
+        std::string text;
+        std::stack<std::string> undoStack;
+};
+
+#endif;
