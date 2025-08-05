8cd49c13019e8ad74b2a526f73a32fe056f2ffcf
Donovan Vernon <d.vernon5@gmail.com>
2025-08-04 18:33:03 -0700

    Ran the program in main function

--- main.cpp
+++ main.cpp
@@ -1,43 +1,10 @@
 #include <iostream>
 #include <string>
 #include <stack>
+#include "request_manager.hpp"
 
-class TextEditor {
-    public:
-        TextEditor(): text("") {};
-
-        std::string append(const std::string& str) {
-            undoStack.push(text);
-            text += str;
-            return text;
-        }
-        
-        std::string deleteChar(size_t index) {
-            undoStack.push(text);
-            if (index > text.length()) {
-                text = "";
-            } else {
-                text = text.erase(text.length() - index);
-            }
-        }
-
-        void printChars(size_t count) {
-            if (count > text.length()) {
-                std::cerr << "The text is too short.\n";
-            }
-            else {
-                std::cout << text[count - 1] << "\n";
-            }
-        }
-
-        void undo() {
-            if (!undoStack.empty()) { 
-                text = undoStack.top();
-                undoStack.pop();
-            }
-        }
-
-    private:
-        std::string text;
-        std::stack<std::string> undoStack;
-};
+int main() {
+    RequestManager requestManager;
+    requestManager.processQueries();
+    return 0;   
+}
